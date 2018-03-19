eb5d2e394b8c4913d82abe3ce9d8bdd7661d1430
Andrey Lukyanov <alukianov@envionsoftware.com>
2018-03-19 15:29:35 +0300

    changesets

--- credorg/offer/utils.py
+++ credorg/offer/utils.py
@@ -235,30 +235,3 @@ class FixturesGenerator(object):
         self.generate_offers(offers_count)
         self.generate_worksheets(worksheets_count)
         self.generate_orders(orders_count)
-
-
-def additional_schema(schema):
-    """
-    rest framework does not provide a way to override the schema on
-    a per endpoint basis.  this is a cheap way to get the fields to
-    appear on the swagger web interface through the filter_backends
-    """
-    class SwaggerFilterBackend(object):
-
-        def get_schema_fields(self, view):
-            keys = ('name', 'required', 'location', 'type', 'description')
-            return [
-                coreapi.Field(**{
-                    key: val for key, val in field.items() if key in keys})
-                for field in schema.values()
-            ]
-
-        def filter_queryset(self, request, queryset, view):
-            # do nothing
-            return queryset
-
-    def wrapper(cls):
-        cls.filter_backends = list(cls.filter_backends) + [SwaggerFilterBackend]
-        return cls
-
-    return wrapper
